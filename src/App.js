c1b5c9a5bf7fedd78476a64e28f0782a4a29fcc5
Sergey Grigorenko <miraigajettolab@gmail.com>
2020-11-20 17:21:51 +0300

    Create basic app that displays the result of an api request

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <p>TestTestTest</p>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+		super(props);
+		this.state = {
+      GetResponse : {}
+    };
+    this.GetIt = this.GetIt.bind(this)
+  }
+
+  GetIt(handle) {
+    console.log(`Making a GET request to ${handle}`)
+    fetch(handle, {
+      method: 'GET',
+      headers: {
+        'Access-Control-Allow-Headers': '*',
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      }
+    }).then(response => response.json()).then(data => this.setState({"GetResponse":data}))
+  }
+
+  componentDidMount() {
+    this.GetIt("https://bank-api.azurewebsites.net/")
+	}
+
+  render() {
+    return (
+      <div className="App">
+        <p>{JSON.stringify(this.state.GetResponse)}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
